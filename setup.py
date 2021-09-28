bd83802066c36ce60296b416db52576503b32e39
Samuel Wilson <44655289+AnotherSamWilson@users.noreply.github.com>
2021-09-28 16:08:16 -0400

    Remove 3.6 support, incompatible with scipy

--- setup.py
+++ setup.py
@@ -36,12 +36,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
