c7f8f5c5461d198da82b80b8651c5d112592846e
Samuel Wilson <44655289+AnotherSamWilson@users.noreply.github.com>
2021-12-08 15:24:14 -0500

    Removed depreciated functions and modules

--- miceforest/__init__.py
+++ miceforest/__init__.py
@@ -1,19 +1,11 @@
 from .utils import ampute_data
-from .ImputedDataSet import ImputedDataSet
-from .MultipleImputedDataSet import MultipleImputedDataSet
-from .MultipleImputedKernel import MultipleImputedKernel
-from .KernelDataSet import KernelDataSet
 from .ImputedData import ImputedData
 from .ImputationKernel import ImputationKernel
 
-__version__ = "5.1.2"
+__version__ = "5.2.0"
 
 __all__ = [
     "ImputedData",
     "ImputationKernel",
-    "MultipleImputedKernel",
-    "ImputedDataSet",
-    "MultipleImputedDataSet",
     "ampute_data",
-    "KernelDataSet",
 ]
