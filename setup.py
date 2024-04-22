68be7898338141a38315a6e73806e5d6a4417e06
Franz Hüttinger <franz.huettinger@gmx.de>
2024-04-22 10:54:41 +0200

    Minimum Python version increased to 3.9

--- setup.py
+++ setup.py
@@ -40,8 +40,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
